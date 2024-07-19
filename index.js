68749c001742ec6fd64fa4a5cb29522b695e6b47
NadiaGallini <gallini.nadi@yandex.ru>
2024-07-19 10:46:56 +0300

    Добавлен функционал комнат в чат по заданию 16.3

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { Server } = require('socket.io');
 const fs = require('fs');
 const path = require('path');
 
-// Создаем HTTP сервер
 const httpServer = createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/') {
     const filePath = path.join(__dirname, 'views', 'index.html');
@@ -22,34 +21,30 @@ const httpServer = createServer((req, res) => {
   }
 });
 
-// Создаем Socket.IO сервер
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
   }
 });
 
-const messages = ['Hello world!', 'New Message'];
-
 io.on('connection', (socket) => {
-    console.log('New client connected');
-    
-    // Отправка старых сообщений новым клиентам
-    messages.forEach((message) => {
-      socket.emit('message', message);
-    });
-  
-    // Обработка новых сообщений от клиентов
-    socket.on('message', (message) => {
-      console.log('Received message:', message);
-      messages.push(message); // Добавляем новое сообщение в массив
-      io.emit('message', message); // Отправляем сообщение всем клиентам
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('Client disconnected');
-    });
+  console.log('New client connected');
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room);
+    console.log(`Client joined room: ${room}`);
   });
+
+  socket.on('message', (data) => {
+    const { room, message } = data;
+    console.log(`Received message: ${message} in room: ${room}`);
+    io.to(room).emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
 
 httpServer.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
